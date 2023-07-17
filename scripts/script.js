1d129027c808989b2a0dd43f4b87d08bc7d7cecb
root <yassinekhayri123@gmail.com>
2023-07-17 12:46:22 +0100

    Carousel added

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,33 @@
 const buttons = document.getElementsByClassName("nav");
-console.log(buttons);
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", () => {
-    const slider =
-      buttons[i].parentElement.getElementsByClassName("card_cont")[0];
-    const pos = ["flex-start", "space-around", "flex-end"];
+    const slider = buttons[i].parentElement.getElementsByClassName("cards")[0];
     let coef = 1;
+    const parrentW = slider.parentElement.offsetWidth;
+    const padding = (parrentW - 250) / 2;
+    if (!slider.style.left) slider.style.left = `${padding}px`;
 
     if (i % 2) coef = -1;
-
-    if (pos.indexOf(slider.style.justifyContent) === -1)
-      slider.style.justifyContent = pos[1];
-    const index = pos.indexOf(slider.style.justifyContent) + coef;
-
-    if (index > -1 && index < 3) slider.style.justifyContent = pos[index];
-    if (index === -1) slider.style.justifyContent = pos[2];
-    if (index === 3) slider.style.justifyContent = pos[0];
+    const left = slider.style.left.slice(0, -2);
+    if (coef < 0 && left < 0) slider.style.left = `${+left + parrentW}px`;
+    else if (coef > 0 && left > -parrentW)
+      slider.style.left = `${+left - parrentW}px`;
+    else if (coef > 0 && left < -parrentW) slider.style.left = `${padding}px`;
+    else if (coef < 0 && left > 0)
+      slider.style.left = `${+left - parrentW * 2}px`;
   });
+
+  window.onresize = () => {
+    const cards = document.querySelectorAll(".cards");
+    if (window.innerWidth > 980)
+      cards.forEach((cont) => {
+        cont.style.left = "0px";
+      });
+    else {
+      cards.forEach((cont) => {
+        cont.style.left = `${(cont.parentElement.offsetWidth - 250) / 2}px`;
+      });
+    }
+  };
 }
