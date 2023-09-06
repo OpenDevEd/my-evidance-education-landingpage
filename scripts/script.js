aee8b1b44d434724ca4b541106da47f0ed89105c
Yassine-K <yassinekhayri123@gmail.com>
2023-09-06 15:29:26 +0100

    Multi card scroll

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,10 @@ const buttons = document.getElementsByClassName("nav");
 const cards_cont = document.querySelectorAll(".cards");
 const cards = cards_cont[0].getElementsByClassName('card');
 const cards_ref = cards_cont[1].getElementsByClassName('card');
-
+let f;
+let space = 0;
 const resize_cards = () => {
-	let f = cards_ref[0].offsetWidth;
+	f = cards_ref[0].offsetWidth;
 	for(d of cards)
 	{
 		if (d.offsetWidth !== f)
@@ -14,8 +15,19 @@ const resize_cards = () => {
 	{
 		cards_cont[1].style.justifyContent = 'space-between';
 		cards_cont[1].style.left = "0px";
-		cards_cont[0].style.left = `${(cards_cont[0].parentElement.offsetWidth - f) / 2}px`;
-		cards_cont[0].style.gap = `${(cards_cont[0].parentElement.offsetWidth - f)}px`;
+		cards_cont[0].style.maxWidth = `${cards_cont[1].scrollWidth}px`;
+		if (window.innerWidth > 1284)
+		{
+			cards_cont[0].style.gap = `60px`;
+			cards_cont[0].style.left = `20px`;
+		}
+		else
+		{
+			let gap_size = (cards_cont[0].parentElement.offsetWidth - f * 2) / 3 + 20;
+			cards_cont[0].style.gap = `${gap_size}px`;
+			let total_card = cards_cont[0].getElementsByClassName('card').length;
+			cards_cont[0].style.left = `${gap_size - gap_size / total_card}px`;
+		}
 	}
     else {
       cards_cont.forEach((cont) => {
@@ -27,33 +39,51 @@ const resize_cards = () => {
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", () => {
     const slider = buttons[i].parentElement.getElementsByClassName("cards")[0];
-    const parrentOffW = slider.parentElement.offsetWidth;
-    const parrentW = slider.scrollWidth;
+    let parentOffW = slider.parentElement.offsetWidth;
+    const parentW = slider.scrollWidth;
 	const child_count = slider.getElementsByClassName('card').length;
 	let card_width = 250;
-    if (window.innerWidth > 980)
-		card_width = slider.firstElementChild.offsetWidth;
-	console.log(parrentOffW,card_width)
-    const padding = (parrentOffW - card_width) / 2;
-    let coef = 1;
+    let padding = (parentOffW - card_width) / 2;
 
     if (!slider.style.left)
 		slider.style.left = `${padding}px`;
+	const left = slider.style.left.slice(0, -2);
+	let go_back = +left - parentOffW * (child_count - 1);
+    let coef = 1;
 
     if (i % 2)
 		coef = -1;
+	space = 0;
 
-    const left = slider.style.left.slice(0, -2);
-	console.log(left, parrentOffW, +left - parrentOffW)
+    if (window.innerWidth > 980)
+	{
+		card_width = slider.firstElementChild.offsetWidth;
+		if (window.innerWidth > 1184)
+		{
+			padding = 20;
+			go_back = -parentOffW;
+			if (coef < 0 && left < 0)
+				parentOffW += 20;
+		}
+		else
+		{
+			padding = (cards_cont[0].parentElement.offsetWidth - f * 2) / 3 + 20;
+			go_back = -parentOffW - f * 2 -padding + padding / 3 + 10;
+			if ((coef > 0 && left > -parentW + parentOffW) || (coef < 0 && left < 0))
+				space =  -padding / 2 - 10;
+			padding = `${padding - padding / child_count}`;
+		}
+	}
+
     if (coef < 0 && left < 0)
-		slider.style.left = `${+left + parrentOffW}px`;
-    else if (coef > 0 && left > -parrentW + parrentOffW)
-      slider.style.left = `${+left - parrentOffW}px`;//
-    else if (coef > 0 && left < -parrentW + parrentOffW)
+		slider.style.left = `${+left + parentOffW + space}px`;
+    else if (coef > 0 && left > -parentW + parentOffW)
+      slider.style.left = `${+left - parentOffW - space}px`;
+    else if (coef > 0 && left < -parentW + parentOffW)
 		slider.style.left = `${padding}px`;
-    else if (coef < 0 && left > 0)
-    	slider.style.left = `${+left - parrentOffW * (child_count - 1)}px`;
-  });
+    else if (coef < 0 && left >= 0)
+    	slider.style.left = `${go_back}px`;
+	});
 
   window.onresize = () => {
     resize_cards();
